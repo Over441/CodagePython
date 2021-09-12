a9d1d7602c7dce0377bdd3963b9fe33e9ac95190
Over441 <dessart.loo@orange.fr>
2021-09-12 17:19:14 +0200

    Modif

---
+++ password_generator.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+# Créer la fennetre
+window = Tk()
+window.title("Password Générator")
+window.geometry("720x480")
+window.iconbitmap("zevent fond.ico")
+window.config(background='#4065a4')
+
+# Afficher la fenetre
+window.mainloop()
--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 from tkinter import *
+import webbrowser
+
+def open_over44_channel():
+    webbrowser.open_new("https://www.youtube.com/channel/UCfA_pq-YZo8yWDAm6MrkzqQ")
 
 # Création d'un première fenètre
 window = Tk()
@@ -9,6 +13,24 @@ window.geometry("1080x720")
 window.minsize(480, 360)
 window.iconbitmap("zevent fond.ico")
 window.config(background='#41B77F')
+
+# Création d'un boite
+frame = Frame(window, bg='#41B77F')
+
+# Ajout d'un texte
+label_title = Label(frame, text="Bienvenue sur l'application", font=("Dimbo", 40), bg='#41B77F', fg='white')
+label_title.pack()
+
+# Ajout d'un second texte
+label_subtitle = Label(frame, text="Hey salut à toi !", font=("Dimbo", 25), bg='#41B77F', fg='white')
+label_subtitle.pack()
+
+# Ajout d'un boutton
+yt_button = Button(frame, text="Ouvrir Youtube", font=("Dimbo", 25), bg='white', fg='#41B77F', command=open_over44_channel)
+yt_button.pack(pady=25, fill=X)
+
+# Pack la frame
+frame.pack(expand=True)
 
 # afficher la fenettre
 window.mainloop()
